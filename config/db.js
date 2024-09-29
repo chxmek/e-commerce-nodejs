f99312960d1dc573528346f95ffd4556b3758e39
chxmek <Chawanwits.mek@gmail.com>
2024-09-30 02:08:51 +0700

    add db deploy

--- config/db.js
+++ config/db.js
@@ -2,7 +2,8 @@ const mongoose = require("mongoose");
 
 async function connectDB() {
   try {
-    await mongoose.connect(process.env.DATABASE);
+    // await mongoose.connect(process.env.DATABASE);
+    await mongoose.connect(process.env.DATABASE_DEPLOY);
     console.log("Connect DB success");
   } catch (err) {
     console.log(err);
